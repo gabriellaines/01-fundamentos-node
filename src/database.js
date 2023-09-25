f7f9dd58aaf90a09dc1a14b262806cd43a84085f
Gabe Laines <gabrielantonio20111@hotmail.com>
2023-09-25 03:45:21 -0300

    - added filter by query string for user list

--- src/database.js
+++ src/database.js
@@ -26,8 +26,18 @@ export class Database {
         this.#persist();
     }
 
-    select(table) {
-        return this.#database[table] ?? [];
+    select(table, search) {
+        let data = this.#database[table] ?? [];
+
+        if (search) {
+            data = data.filter(row => {
+                return Object.entries(search).some(([key, value]) => {
+                    return row[key].includes(value);
+                });
+            });
+        }
+
+        return data;
     }
 
     delete(table, id) {
